ae7bd310091a5905e2339e8403d2e06c031d917c
dmamchyts <dmitry.mamchyts@hitapps.com>
2022-11-04 09:11:22 +0300

    add removeEntityProxyWrapper into EntityUserProvider

--- src/DependencyInjection/Security/EntityUserProvider.php
+++ src/DependencyInjection/Security/EntityUserProvider.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Cycle\SymfonyBundle\DependencyInjection\Security;
 
-use Cycle\ORM\{ORMInterface, RepositoryInterface};
+use Cycle\ORM\{EntityProxyInterface, ORMInterface, RepositoryInterface};
 use Cycle\SymfonyBundle\Exception\AbstractException;
 use Symfony\Component\Security\Core\Exception\{UnsupportedUserException, UserNotFoundException};
 use Symfony\Component\Security\Core\User\{PasswordAuthenticatedUserInterface, PasswordUpgraderInterface, UserInterface, UserProviderInterface};
@@ -32,7 +32,7 @@ class EntityUserProvider implements UserProviderInterface, PasswordUpgraderInter
             throw $e;
         }
 
-        return $user;
+        return $this->removeEntityProxyWrapper($user);
     }
 
     /**
@@ -64,7 +64,7 @@ class EntityUserProvider implements UserProviderInterface, PasswordUpgraderInter
             }
         }
 
-        return $refreshedUser;
+        return $this->removeEntityProxyWrapper($refreshedUser);
     }
 
     /**
@@ -98,7 +98,42 @@ class EntityUserProvider implements UserProviderInterface, PasswordUpgraderInter
 
     private function getUserPkValue(UserInterface $user): string|int
     {
-        // @toto find new method for detection if PK value
+        // @todo find new method for detection PK value
         return method_exists($user, 'getId') ? $user->getId() : throw new AbstractException('Can not get PK value');
+    }
+
+    /**
+     * @todo this is method is a hack for removing EntityProxyInterface from user entity
+     *       symfony use serialize/unserialize for authentication token
+     *       and as result unserialize does not work correctly with EntityProxyInterface wrapper
+     */
+    private function removeEntityProxyWrapper(UserInterface $user): UserInterface
+    {
+        if (!$user instanceof EntityProxyInterface) {
+            return $user;
+        }
+
+        /** @var \ReflectionClass */
+        $parentReflection = (new \ReflectionClass($user))->getParentClass();
+        $parentClassName = $parentReflection->getName();
+
+        /** @var UserInterface */
+        $originalUser = new $parentClassName();
+        $originalReflection = new \ReflectionClass($originalUser);
+
+        // set properties from parent to `$originalUser`
+        foreach ($parentReflection->getProperties() as $parentProperty) {
+            foreach ($originalReflection->getProperties() as $originalProperty) {
+                if ($originalProperty->getName() !== $parentProperty->getName()) {
+                    continue;
+                }
+
+                $originalProperty->setValue($originalUser, $parentProperty->getValue($user));
+
+                break;
+            }
+        }
+
+        return $originalUser;
     }
 }
